3e0a650dc19bbe711380e27ac617800dcd16acc1
havuongquoc2000@gmail.com <havuongquoc2000@gmail.com>
2020-06-17 17:54:53 +0700

    Add upload file

---
+++ upload.php
@@ -0,0 +1,22 @@
+<?php 
+    $connect = new mysqli("localhost","root","","");
+
+
+    if(isset($_POST["submit"])){
+        $author = $_POST["author"];
+        $emailauthor = $_POST["emailauthor"];
+        $pname = rand(1000,10000)."-".$_FILES["file"]["name"];
+
+        $tname = $_FILES["files"]["tmp_name"];
+        $upload_dir = '/images';
+        move_uploaded_file($tname,$upload_dir."/".$pname);
+        $sql = "INSERT INTO dtbName ('','','') VALUES ('','','') ;"; 
+
+        if($connect->query($sql)){
+            echo "Uploading file success";
+        }else{
+            die("Fail to upload file");
+        }
+    }
+    $connect->close();
+?>
