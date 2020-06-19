0c9718c70ce1db0a2e39026d00cc54761ee93c26
QUAN PHAM <andojohn2709@gmail.com>
2020-06-19 15:14:14 +0700

    i just added my part oke abes

---
+++ js/ts.js
@@ -0,0 +1,42 @@
+
+    document.getElementById('modal-btn').addEventListener('click', function() {
+        document.querySelector('.modal-bg').style.visibility = 'visible';
+    });
+     document.getElementById('close').addEventListener('click',
+     function(){
+         document.querySelector('.modal-bg').style.visibility = ' hidden'; 
+     })
+  
+    
+     document.getElementById('modal-btn2').addEventListener('click', function() {
+        document.querySelector('.modal-bg2').style.visibility = 'visible';
+    });
+     document.getElementById('close2').addEventListener('click',
+     function(){
+         document.querySelector('.modal-bg2').style.visibility = ' hidden'; 
+     })
+  
+     document.getElementById('modal-btn3').addEventListener('click', function() {
+        document.querySelector('.modal-bg3').style.visibility = 'visible';
+    });
+     document.getElementById('close3').addEventListener('click',
+     function(){
+         document.querySelector('.modal-bg3').style.visibility = ' hidden'; 
+     })
+    
+  
+     document.getElementById('modal-btn4').addEventListener('click', function() {
+        document.querySelector('.modal-bg4').style.visibility = 'visible';
+    });
+     document.getElementById('close4').addEventListener('click',
+     function(){
+         document.querySelector('.modal-bg4').style.visibility = ' hidden'; 
+     })
+    
+     document.getElementById('modal-btn5').addEventListener('click', function() {
+        document.querySelector('.modal-bg5').style.visibility = 'visible';
+    });
+     document.getElementById('close5').addEventListener('click',
+     function(){
+         document.querySelector('.modal-bg5').style.visibility = ' hidden'; 
+     })
